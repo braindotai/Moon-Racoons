fd0393be6310288748db0689af063be5a4e68151
Rishik Mourya <braindotai@gmail.com>
2020-11-21 23:45:15 +0530

    error test 2

--- MoonRacoons/MoonRacoons/wsgi.py
+++ MoonRacoons/MoonRacoons/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoonRacoons.MoonRacoons.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoonRacoons.settings')
 
 application = get_wsgi_application()
